3b968230cea5c7ae8489e4dea335b3fbd45482f4
nick747 <nickodon747@proton.me>
2024-06-16 15:13:22 +0200

    init commit - setup project

---
+++ tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        name: "2.25rem",
+        subtitle: "2rem",
+        body: "1rem",
+        highlights: "1.5rem",
+      },
+      textColor: {
+        normal: "#DEDEDE",
+        hightlighted: "#B3A7FF",
+      },
+    },
+  },
+  plugins: [],
+};
